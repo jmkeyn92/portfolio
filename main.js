83e40148b1a38d1586ea5e7046a2bd104c329e26
jmkeyn92 <82368439+jmkeyn92@users.noreply.github.com>
2022-03-17 19:44:01 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -21,9 +21,16 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: 'smooth'});
-  
+  scrollIntoView(link);
 });
 
+// Handle click on "contact me" button on home
+const contactButton = document.querySelector('.home__contact');
+contactButton.addEventListener('click', (event) => {
+  scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+};
